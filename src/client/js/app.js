28bd41b27b92df5c660ff7aa3cb5175df721b1f4
AndreaScacchi <andreascacchi10@gmail.com>
2021-11-27 20:11:39 +0100

    Countdown created

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -96,6 +96,25 @@ const postTravelData = async (url = '', data = {}) => {
     }
 };
 
+// The countdown
+const countDownDate = new Date().getTime();
+
+const x = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("demo").innerHTML = days + "d" + hours + "h" + minutes + "m" + seconds + "s";
+
+    if(distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "BON VOYAGE";
+    }
+}, 1000);
+
 
 
 
