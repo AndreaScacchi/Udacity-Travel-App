23f252ed78c2113cd7a64376404a12788d0ea69d
AndreaScacchi <andreascacchi10@gmail.com>
2021-11-28 18:10:12 +0100

    Import functions in index.js file

---
+++ src/client/index.js
@@ -0,0 +1,7 @@
+import { handleSubmit } from './js/app';
+import { getCityInfo } from './js/app';
+import { getWeather } from './js/app';
+import { postTravelData } from './js/app';
+import { countDownDate } from './js/app';
+import { updateUI } from './js/app';
+import "./styles/style.scss";
