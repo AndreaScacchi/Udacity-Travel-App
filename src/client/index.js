2166be8d7f312871fae1acad029e2398228c5eb5
AndreaScacchi <andreascacchi10@gmail.com>
2021-11-28 18:43:24 +0100

    Print or cancel the trip

--- src/client/index.js
+++ src/client/index.js
@@ -5,3 +5,22 @@ import { postTravelData } from './js/app';
 import { countDownDate } from './js/app';
 import { updateUI } from './js/app';
 import "./styles/style.scss";
+
+// Print ar cancel the trip
+document.addEventListener('DOMContentLoaded', (event) => {
+    event.preventDefault();
+
+    const print = document.getElementById('print');
+    const cancel = document.getElementById('delete');
+
+    print.addEventListener('click', () => {
+        window.print();
+        location.reload();
+    });
+
+    cancel.addEventListener('click', () => {
+        form.reset();
+        results.classList.add('hidden');
+        location.reload();
+    });
+});
