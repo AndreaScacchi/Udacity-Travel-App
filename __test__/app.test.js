22a83e43475883633ce9d3716bf8e14ba7a7110d
AndreaScacchi <andreascacchi10@gmail.com>
2021-11-28 18:48:31 +0100

    Starting the test

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -1,1 +1,6 @@
 import { handleSubmit } from './js/app';
+describe("Testing the submit functionality", function() {
+    test("Testing the handleSubmit() function", function() {
+        expect(handleSubmit).toBeDefined();
+    });
+});
