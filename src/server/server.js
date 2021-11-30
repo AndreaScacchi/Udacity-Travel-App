e527dbde2a7c250f0f7d7a458e19f93a0e9c91ae
AndreaScacchi <andreascacchi10@gmail.com>
2021-11-30 22:18:06 +0100

    dotenv.config() added

--- src/server/server.js
+++ src/server/server.js
@@ -1,32 +1,35 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 let journeyData = {};
 
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-app.use(express.static('dist'));
+app.use(express.static("dist"));
 
 // Server
 const port = 3000;
 app.listen(port, () => {
-    console.log(`The server is listening on port ${port}...`);
+  console.log(`The server is listening on port ${port}...`);
 });
 
 // GET routes
-app.get('/all', (req, res) => {
-    res.sendFile('dist/index.html');
+app.get("/all", (req, res) => {
+  res.sendFile("dist/index.html");
 });
 
-app.get('/test', async (req, res) => {
-    res.json();
+app.get("/test", async (req, res) => {
+  res.json();
 });
 
 // POST routes
-app.post('/add', (req, res) => {
-    journeyData = req.body;
-    res.send(journeyData);
+app.post("/add", (req, res) => {
+  journeyData = req.body;
+  res.send(journeyData);
 });
 
 module.exports = app;
